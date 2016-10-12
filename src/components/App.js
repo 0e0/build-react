262016668f2ded40a483668e1773809a0480a7d0
0e0 <ejersonb@gmail.com>
2016-10-12 14:15:22 -0500

    added css-loader

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>What do I want to learn today?</div>
+      <div className='element'>What do I want to learn today?</div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/app/index.html',
+  template: __dirname + '/src/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
+
 module.exports = {
-  entry: __dirname + '/app/index.js',
+  entry: __dirname + '/src/index.js',
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel'
+      },
+      {
+        test: /\.css/,
+        loaders: ['style', 'css'],
+        include: __dirname + '/src'
       }
     ]
   },
   output: {
-    filename: 'transformed.js',
+    filename: 'bundle.js',
     path: __dirname + '/build'
   },
   plugins: [HTMLWebpackPluginConfig]

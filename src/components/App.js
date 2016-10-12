fe27c0089e583b7c9384404a361a4401785c7169
0e0 <ejersonb@gmail.com>
2016-10-12 16:00:52 -0500

    added less

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,16 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='element'>What do I want to learn today?</div>
+      <div>
+        <div className='element'>
+          <h3>What do I want to learn today? </h3>
+          <p>I am really thankful for being able to learn how to code!</p>
+        </div>
+        <div className='great'>
+          <h3>How do I improve my day to day learning?</h3>
+          <p>I need to go through the process I created today!</p>
+        </div>
+      </div>
     );
   }
 }

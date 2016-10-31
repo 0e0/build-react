55bd2d6634abc28ccf57f342961de82c294349df
0e0 <ejersonb@gmail.com>
2016-10-31 12:12:17 -0500

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import Tabs from './tabs';
+import { Link } from 'react-router';
+import { stack as Menu } from 'react-burger-menu';
+import Radium from 'radium';
+
+import NavLink from './NavLink';
+let RadiumLink = Radium(Link);
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div className='element'>
-          <h3>What do I want to learn today? </h3>
-          <p>I am really thankful for being able to learn how to code!</p>
+        <div>
+          <h1 className="title">Operation Spread the Music</h1>
         </div>
-        <div className='great'>
-          <h3>How do I improve my day to day learning?</h3>
-          <p>I need to go through the process I created today!</p>
+        <div id="outer-container">
+          <Menu left width={ 280 } outerContainerId={ "outer-container" }>
+            <RadiumLink className="bm-item-list" to="/"></RadiumLink>
+            <RadiumLink className="bm-item-list" to="/about">About</RadiumLink>
+            <RadiumLink className="bm-item-list" to="/join">Join</RadiumLink>
+            <RadiumLink className="bm-item-list" to="/events">Events</RadiumLink>
+            <RadiumLink className="bm-item-list" to="/resources">Resources</RadiumLink>
+          </Menu>
+          {this.props.children}
+
+          <div className="footer">
+          <div>
+            <h2>I really need to time myself so I do not forget to eat or exercise.</h2>
+          </div>
+            <h1>I am a footer!</h1>
+          </div>
+
         </div>
-        <Tabs />
       </div>
 
     );
